cc3ec8718112c5edcde49c55512cb9b3b0e34610
Pushkar <p.kulkarni0007@gmail.com>
2026-04-02 09:47:24 +0000

    added springboot project

---
+++ backend/src/main/java/com/pushkar/collabcode/CollabcodeApplication.java
@@ -0,0 +1,13 @@
+package com.pushkar.collabcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabcodeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pushkar/collabcode/CollabcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pushkar.collabcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
